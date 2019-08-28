5d4e20a3c9bd7f1b15a8d546ee8404d19bad4ffa
Perpo Hipolito <hperpo@gmail.com>
2019-08-28 20:57:03 +0800

    add keyboard interupt

--- parallel-upload.py
+++ parallel-upload.py
@@ -1,21 +1,18 @@
 import os
-import sys
 import time
 import requests
 import json
 from requests_toolbelt import MultipartEncoder
 
-from multiprocessing import Process, Semaphore, Manager
+from multiprocessing import Process, Semaphore, Manager, Value
 from threading import Thread
 
 from orator import DatabaseManager
 from db_config import DATABASES
 
-from shutil import copyfile
 
-
-MAX_CONTRACTOR_WORKERS = 25
-MAX_WRITER_WORKERS = 5
+MAX_CONTRACTOR_WORKERS = 30
+MAX_WRITER_WORKERS = 10
 
 #task status
 PENDING = 0
@@ -23,73 +20,54 @@ PROCESSING = 1
 SUCCESS = 2
 FAILED = 3
 
-#nginx basic auth
-USERNAME = 'user'
-PASSWORD = 'password'
+#nginx basic auth since our ipfs-s3 server need credentials to access the resources
+USERNAME = 'pygmalion1357'
+PASSWORD = 'galatea2468'
 
-#test
-def get_all_images(): #return list
-	
-	image_path = os.path.join(os.getcwd(), "images")
-	dog_image = os.path.join(image_path, "dog.jpg")
 
-	images = []
-
-	for i in range(10):
-		dest = os.path.join(image_path, '%s.jpg' %i)
-		copyfile(dog_image, dest)
-		images.append("%s" %(dest))
-
-	return images
-
-class Loader(Process):
-	
-	def __init__(self):
-		super(Loader, self).__init__()
-		self.db = DatabaseManager(DATABASES)
-	
-	def run(self): 
-		
-		print("Loader online")
-
-		images = get_all_images() ##get all images in list and put that images in the database and set to new PENDING
-		if images:
-			for image in images:
-				self.db.table("tasks").insert({'image_path': image})
-
-		
 class Contractor(Process):
-	
+
 	def __init__(self, queue):
 		super(Contractor, self).__init__()
 		self.worker_control = Semaphore(MAX_CONTRACTOR_WORKERS)
 		self.db = DatabaseManager(DATABASES)
 		self.result_queue = queue
 
+	def reset_processing_to_pending(self):
+		self.db.table("tasks").where('processing_status', PROCESSING).update({'processing_status' : PENDING})
+
 	def run(self):
-		
-		print("Contractor online")
+
+		print("*** Contractor online")
+
+		print("**** Resetting all halted tasks which status is processing into new pending")
+		self.reset_processing_to_pending()
 
 		while True:
-			if self.worker_control.acquire(False):
 
+			if self.worker_control.acquire(False):
 				try:
 					#get pending task
 					task = self.db.table('tasks').where('processing_status', PENDING).first()
-					
+
 					if task:
 						self.db.table("tasks").where('id', task.id).update({'processing_status' : PROCESSING})
 
 						worker = ContractorWorker(task, self.worker_control, self.result_queue)
 						worker.start()
 
-				except Exception as err:
-					print(err)	
+					else:
+						self.worker_control.release()
 
-			time.sleep(0.2)		
+				except Exception as err:
+					print(err)
+					self.worker_control.release()
+
+			time.sleep(0.2)
+
 
 class ContractorWorker(Thread):
-	
+
 	def __init__(self, task, worker_control, queue):
 		Thread.__init__(self)
 		self.task = task
@@ -105,35 +83,32 @@ class ContractorWorker(Thread):
 		try:
 			m = MultipartEncoder( fields={'file': ('filename', open(file_path, 'rb'), 'application/octet-stream')})
 			r = requests.post("https://ipfs-s3.shortcut-lab.com/api/v0/add", data=m, headers={'Content-Type': m.content_type},auth=self.ipfs_s3_auth)
-		
+
 			if r.status_code == 200:
 				file_hash = json.loads(r.text)["Hash"]
 			else:
-				raise Exception("IPFS FILE UPLOAD FAILED")
+				raise Exception("--- IPFS FILE UPLOAD FAILED FOR IMAGE FILEPATH %s" %(file_path))
 
 		except Exception as err:
 			print(err)
 			status = False
 
 		return(status, file_hash)
-			
+
 	def run(self):
 		#execute task
-		print("contractor worker for task: %s" % self.task)
+		print("** Contractor worker alive for task-id: %s" % self.task.id)
 		#get image path
 		task_id = self.task.id
 		file_path = self.task.image_path
-		
-		try:			
-			print("Uploading file `%s` to ipfs" % file_path)
+
+		try:
+			print("--- Uploading file `%s` to ipfs server with task id of %s \n" % (file_path, task_id))
 
 			status, ipfs_hash = self.upload_file(file_path)
-			#set status to sucess
-			#self.db.table("tasks").where('id', task_id).update({'processing_status': SUCCESS})
-
 			processing_status = SUCCESS
 
-			print("filepath: %s , status: %s" %(file_path, status))
+			#print("filepath: %s , status: %s" %(file_path, status))
 
 			if not status:
 				ipfs_hash = None
@@ -142,81 +117,105 @@ class ContractorWorker(Thread):
 			self.result_queue.put({'id': task_id,'ipfs_hash': ipfs_hash ,'processing_status': processing_status})
 
 		except Exception as err:
-			print("contractor exception %s" % err)
-			#set status to failure
-			#self.db.table("tasks").where('id', task_id).update({'processing_status': FAILED})
+			print("--- Contractor exception %s" % err)
 			self.result_queue.put({'id': task_id, 'ipfs_hash': ipfs_hash , 'processing_status': FAILED})
 
-		self.worker_control.release() 
-
-
+		self.worker_control.release()
 
 class DbWriter(Process):
-	
+
 	def __init__(self, queue):
 		super(DbWriter, self).__init__()
 		self.worker_control = Semaphore(MAX_WRITER_WORKERS)
 		self.result_queue = queue
 
 	def run(self):
-		
-		print("DB Writer online")
+
+		print("*** DB Writer online")
 
 		while True:
 
 			if self.worker_control.acquire(False):
 
 				task = self.result_queue.get()
-				
+
 				if task:
 					try:
-						worker = WriterWorker(task, self.worker_control) 
+						worker = WriterWorker(task, self.worker_control)
 						worker.start()
 
 					except Exception as err:
 						print(err)
 						print("Invalid task %s" % task)
+						self.worker_control.release()
+				else:
+					self.worker_control.release()
 
-			time.sleep(0.3)			
+			time.sleep(0.3)
 
 class WriterWorker(Thread):
-	
+
 	def __init__(self, task, worker_control):
 		Thread.__init__(self)
 		self.task = task
-		self.worker_control = worker_control	
+		self.worker_control = worker_control
 		self.db = DatabaseManager(DATABASES)
 
 	def update_task_status(self, params):
-		print("Updating processing_status for task %s" % params['id'])
+
+		print("-- Updating processing_status for task id %s with processing_status of %s into the database" % (params['id'], params['processing_status']))
+
 		self.db.table("tasks").where("id", params['id']).update({"ipfs_hash": params["ipfs_hash"], "processing_status": params['processing_status']})
-		print("- Done")
+
+		print("--- Done updating the task-id %s into the database" % params['id'])
 
 	def run(self):
 
-		print("writer worker online for task: %s" % self.task)
+		print("** Writer worker alive for task-id: %s" % self.task["id"])
 
 		try:
 			self.update_task_status(self.task)
-	
+
 		except Exception as err:
 			print(err)
 
 		self.worker_control.release()
 
+class Launcher:
+	def __init__(self):
+		pass
+
+	def start(self):
+
+		loader = None
+		contractor = None
+		writer = None
+
+		try:
+			# START CONTRACTOR AND DBWRITER
+			manager = Manager()
+			queue = manager.Queue()
+
+			contractor = Contractor(queue)
+			contractor.start()
+			contractor_pid = contractor.pid
+			print("contractor pid: %s" %contractor_pid)
+
+			writer = DbWriter(queue)
+			writer.start()
+			writer_pid = writer.pid
+			print("writer pid: %s" % writer_pid)
+
+			# wait for the loader and contractor and writer to exit
+			contractor.join()
+			writer.join()
+
+		except (KeyboardInterrupt, SystemExit): #stop processes immediately,halted task will be handle during the restart of the script
+			print("Stopping all Process. Done.")
+			contractor.terminate()
+			writer.terminate()
 
 if __name__ == "__main__":
 
-	manager = Manager()
-	queue = manager.Queue()
-
-	loader = Loader()
-	contractor = Contractor(queue)
-	writer = DbWriter(queue)
-	loader.start()
-	contractor.start()
-	writer.start()
-
-	loader.join()
-	contractor.join()
-	writer.join()		
+	launcher = Launcher()
+	launcher.start()
